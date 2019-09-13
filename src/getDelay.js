69f61d8155049bc018bab199488cc51518088c5d
Kevin Gonzalez <typekev@gmail.com>
2019-09-13 15:37:36 +0200

    Add documentation for common functions

--- src/getDelay.js
+++ src/getDelay.js
@@ -1,6 +1,14 @@
 import getKeyPressDelay from './getKeyPressDelay';
 import { defaultKeyPressDelay } from './constants';
 
+/**
+ * Return a delay in milliseconds based on a given `action` and `delayRange`
+ *
+ * @param {number|string} action A delay as a number or a string used to calculate a delay
+ * @param {number[]} delayRange An array of two numbers forming a range [min, max]
+ * @returns {number}
+ */
+
 const getDelay = (action, delayRange = defaultKeyPressDelay) =>
   typeof action === 'number' ? action : action.length * getKeyPressDelay(...delayRange);
 
--- src/getKeyPressDelay.js
+++ src/getKeyPressDelay.js
@@ -1,3 +1,11 @@
+/**
+ * Return a random number between `min` and `max`
+ *
+ * @param {number} min The smallest possible value
+ * @param {number} max The largest possible value
+ * @returns {number}
+ */
+
 const getKeyPressDelay = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 export default getKeyPressDelay;
