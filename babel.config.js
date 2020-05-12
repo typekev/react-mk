88f5a26910365bf2f06370c69a589ae685fb8c63
Kevin Gonzalez <typekev@gmail.com>
2020-05-13 00:07:10 +0200

    migrate to babel config js

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: [['styled-jsx/babel', { optimizeForSpeed: true }]],
+};
