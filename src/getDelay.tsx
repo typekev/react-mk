4f5164d6882ff92695688b0994ad7ef9b6feb6f1
Kevin Gonzalez <typekev@gmail.com>
2020-05-16 21:16:28 +0200

    update types of getDelay

--- src/getDelay.tsx
+++ src/getDelay.tsx
@@ -1,14 +1,15 @@
-import getKeyPressDelay from "./getKeyPressDelay";
-import { defaultKeyPressDelay } from "./constants";
+import { Action } from '../types';
+import getKeyPressDelay from './getKeyPressDelay';
+import { defaultKeyPressDelay } from './constants';
+
 /**
- * Return a delay in milliseconds based on a given `action` and `delayRange`
+ * Returns a delay in milliseconds based on a given `action` and `delayRange`
  *
- * @param {number|string} action A delay as a number or a string used to calculate a delay
- * @param {number[]} delayRange An array of two numbers forming a range [min, max]
- * @returns {number}
+ * @param action - The smallest possible output
+ * @param delayRange - The largest possible output
+ * @returns A millisecond delay
  */
-const getDelay = (action, delayRange = defaultKeyPressDelay) =>
-  typeof action === "number"
-    ? action
-    : action.length * getKeyPressDelay(...delayRange);
+const getDelay = (action: Action, delayRange = defaultKeyPressDelay) =>
+  typeof action === 'number' ? action : action.length * getKeyPressDelay(...delayRange);
+
 export default getDelay;
