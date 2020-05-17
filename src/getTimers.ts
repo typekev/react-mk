98c1267f0c0c6db12d1f79cfaf0f0f9d1438a0be
Kevin Gonzalez <typekev@gmail.com>
2020-05-17 23:54:51 +0200

    update getTimers types

--- src/getTimers.ts
+++ src/getTimers.ts
@@ -1,3 +1,4 @@
+import { Action, Range } from "../types";
 import getTimer from './getTimer';
 import getDelay from './getDelay';
 
@@ -6,25 +7,28 @@ export const getPreviousDelay = (delays: number[], index: number) =>
 
 export const accumulateDelays = (
   accumulatedDelays: number[],
-  action: number,
+  action: Action,
   index: number,
-  delayRange: [number, number],
+  delayRange: Range,
 ) => [
-  ...accumulatedDelays,
-  getDelay(action, delayRange) + getPreviousDelay(accumulatedDelays, index),
-];
-export const getDelays = (actions: number[], delayRange: [number, number]) =>
+    ...accumulatedDelays,
+    getDelay(action, delayRange) + getPreviousDelay(accumulatedDelays, index),
+  ];
+
+export const getDelays = (actions: Action[], delayRange: Range) =>
   actions.reduce(
     (accumulatedDelays: number[], action, index) =>
       accumulateDelays(accumulatedDelays, action, index, delayRange),
     [],
   );
-export const startTimers = (actions: number[], then: () => Promise<void>) => (
+
+export const startTimers = (actions: Action[], then: () => Promise<void>) => (
   delay: number,
   index: number,
 ) => getTimer(actions[index], [delay, delay]).then(then);
-const getTimers = (actions: number[], then: () => Promise<void>, delayRange: [number, number]) => {
+const getTimers = (actions: Action[], then: () => Promise<void>, delayRange: Range) => {
   const delays = getDelays(actions, delayRange);
   delays.forEach(startTimers(actions, then));
 };
+
 export default getTimers;
