74ed7cd18ca931a96ff6afa3550dad2a94c05305
Kevin Gonzalez <typekev@gmail.com>
2020-05-17 23:51:19 +0200

    add types to getTimer

--- src/getTimer.ts
+++ src/getTimer.ts
@@ -1,21 +1,28 @@
+import { Action, Range } from "../types";
 import getDelay from "./getDelay";
-export const clearTimer = ({ action, timer }) => {
+
+interface Props { action: Action, timer: NodeJS.Timeout }
+
+export const clearTimer = ({ action, timer }: Props) => {
   clearTimeout(timer);
   return action;
 };
-export const getTimeout = (resolve, action, delay) => {
-  const timer = setTimeout(
-    /* istanbul ignore next */
+
+export const getTimeout = (resolve: (params: Props) => void, action: Action, delay: number) => {
+  const timer: NodeJS.Timeout = setTimeout(
     () => resolve({ action, timer }),
     delay
   );
   return timer;
 };
-export const createTimer = (action, delayRange) => resolve => {
+
+export const createTimer = (action: Action, delayRange: Range) => (resolve: (params: Props) => void) => {
   const delay = getDelay(action, delayRange);
   const timer = getTimeout(resolve, action, delay);
   return { timer, delay };
 };
-const getTimer = (action, delayRange) =>
-  new Promise(createTimer(action, delayRange)).then(clearTimer);
+
+const getTimer = (action: Action, delayRange: Range) =>
+  new Promise<Props>(createTimer(action, delayRange)).then(clearTimer);
+
 export default getTimer;
