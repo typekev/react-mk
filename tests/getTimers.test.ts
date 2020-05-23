ae2708f2a995b651214b9acc52c097622b3768aa
Kevin Gonzalez <typekev@gmail.com>
2020-05-23 09:02:11 +0200

    fix bugs in test

--- tests/getTimers.test.ts
+++ tests/getTimers.test.ts
@@ -7,7 +7,7 @@ import getTimers, {
 
 describe('getTimers function', () => {
   it('should not throw an error when passed an empty array', () => {
-    expect(() => getTimers([], () => {})).not.toThrow();
+    expect(() => getTimers([], () => { })).not.toThrow();
   });
 
   it('should return 10', () => {
@@ -16,22 +16,23 @@ describe('getTimers function', () => {
 
   it('should return an array of length 1', () => {
     expect(
-      [['Test']].reduce((accumulatedDelays, action, index) =>
+      ['Test'].reduce((accumulatedDelays, action, index) =>
         accumulateDelays(accumulatedDelays, action, index, [0, 0]),
+        []
       ).length,
     ).toBe(1);
   });
 
   it('should return an array of length 1', () => {
-    expect(getDelays([['Test']]).length).toBe(1);
+    expect(getDelays(['Test']).length).toBe(1);
   });
 
   it('should not throw an error', () => {
     const text = 'Test';
     const actions = [text, text, text];
-    const then = () => {};
+    const then = () => { };
     const delay = 100;
     const index = 1;
-    expect(() => startTimers(actions, then)(delay, index).length).not.toThrow();
+    expect(() => startTimers(actions, then)(delay, index)).not.toThrow();
   });
 });
