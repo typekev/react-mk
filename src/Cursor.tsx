4e05a3613d95ccb580b0b3187348ae6c37a7617c
Kevin Gonzalez <typekev@gmail.com>
2020-05-31 23:34:23 +0200

    replace styled-jsx with emotion

--- src/Cursor.tsx
+++ src/Cursor.tsx
@@ -1,38 +1,41 @@
-import React, {PropsWithChildren, DetailedHTMLProps} from 'react';
+import React, { PropsWithChildren, DetailedHTMLProps } from 'react';
+import { css, keyframes } from 'emotion';
+
+const blinkAnimation = keyframes`
+  from {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
 
 interface Props extends DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
   blink?: boolean;
-  blinkAnimationDuration?: number,
+  blinkAnimationDuration?: number;
 }
 
-export default function Cursor({ blink = true, blinkAnimationDuration = 700, children = '|', ...rest }: PropsWithChildren<Props>) {
+export default function Cursor({
+  blink = true,
+  blinkAnimationDuration = 700,
+  children = '|',
+  ...rest
+}: PropsWithChildren<Props>) {
   return (
-    <>
-      <span
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...rest}
-      >
-        {children}
-      </span>
-      <style jsx>
-        {`
-          @keyframes cursor-blink {
-            from {
-              opacity: 1;
-            }
-            50% {
-              opacity: 0;
-            }
-            to {
-              opacity: 1;
-            }
-          }
-          span {
-            animation: cursor-blink ${blinkAnimationDuration}ms ${blink ? 'infinite' : 0};
-          }
-        `}
-      </style>
-    </>
+    <span
+      {...rest}
+      className={css`
+        width: 96px;
+        height: 96px;
+        border-radius: 50%;
+        animation: ${blinkAnimation} ${blinkAnimationDuration}ms ${blink ? 'infinite' : 0};
+        transform-origin: center bottom;
+      `}
+    >
+      {children}
+    </span>
   );
 }
-
