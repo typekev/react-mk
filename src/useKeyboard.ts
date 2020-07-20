a07eb0f913cda97d68b0e70df52244862c06d057
Kevin Gonzalez <typekev@gmail.com>
2020-07-20 23:12:02 +0200

    fix bug in clearText

--- src/useKeyboard.ts
+++ src/useKeyboard.ts
@@ -1,16 +1,19 @@
-import { useState, useRef, useEffect, Dispatch, SetStateAction } from "react";
-import { Action, Range } from "../types";
-import getTimers from "./getTimers";
-import getTimer from "./getTimer";
-import { defaultKeyPressDelay } from "./constants";
+import { useState, useRef, useEffect, Dispatch, SetStateAction } from 'react';
+import { Action, Range } from '../types';
+import getTimers from './getTimers';
+import getTimer from './getTimer';
+import { defaultKeyPressDelay } from './constants';
 
-const initialState = [""];
+const initialState: string[] = [];
 
 export const backspace = (chars: string[], setChars: Dispatch<SetStateAction<string[]>>) =>
   setChars(chars.length > 1 ? chars.slice(0, chars.length - 1) : initialState);
 
-export const type = (chars: string[], nextChar: string, setChars: Dispatch<SetStateAction<string[]>>) =>
-  setChars([...chars, nextChar]);
+export const type = (
+  chars: string[],
+  nextChar: string,
+  setChars: Dispatch<SetStateAction<string[]>>,
+) => setChars([...chars, nextChar]);
 
 const useKeyboard = () => {
   const [chars, setChars] = useState<string[]>(initialState);
@@ -27,10 +30,8 @@ const useKeyboard = () => {
       const [nextChar, ...newRemainingChars] = remainingChars;
       const doType = () => type(chars, nextChar, setChars);
       const doSetRemainingChars = () => setRemainingChars(newRemainingChars);
-      getTimer(nextChar, delayRange)
-        .then(doType)
-        .then(doSetRemainingChars);
-    } else if (typeof resolver === "function") {
+      getTimer(nextChar, delayRange).then(doType).then(doSetRemainingChars);
+    } else if (resolver) {
       resolver();
       setResolver(undefined);
       setRemainingChars(initialState);
@@ -38,12 +39,12 @@ const useKeyboard = () => {
   }, [remainingChars]);
 
   const setText = (action: Action, keyPressDelayRange: Range) =>
-    new Promise(resolve => {
+    new Promise((resolve) => {
       setResolver(() => resolve);
       setDelayRange(keyPressDelayRange);
       setChars(initialState);
       /* istanbul ignore else */
-      if (typeof action === "string" && action.length > 0) {
+      if (typeof action === 'string' && action.length > 0) {
         setRemainingChars(action.split(''));
       } else {
         resolve();
@@ -51,25 +52,25 @@ const useKeyboard = () => {
     });
 
   const clearText = (action: Action) =>
-    new Promise<Action>(resolve =>
+    new Promise<Action>((resolve) =>
       /* istanbul ignore next */
-      !chars
+      !chars.length
         ? resolve(action)
         : getTimers(
-          charsRef.current,
-          () => {
-            /* istanbul ignore next */
-            backspace(charsRef.current, setChars);
-            /* istanbul ignore next */
-            return charsRef.current.length === 0 && resolve(action);
-          },
-          delayRange
-        )
+            charsRef.current,
+            () => {
+              /* istanbul ignore next */
+              backspace(charsRef.current, setChars);
+              /* istanbul ignore next */
+              return charsRef.current.length === 0 && resolve(action);
+            },
+            delayRange,
+          ),
     );
 
   const text = chars.join('');
 
   return { text, setText, clearText };
-}
+};
 
-export default useKeyboard
+export default useKeyboard;
